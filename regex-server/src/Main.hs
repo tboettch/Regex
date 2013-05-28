9b41c07d8ffd54c1f81e7eceea9606cb9f7559d0
Tom Boettcher <tom.boettcher@cerner.com>
2013-05-28 15:19:37 -0500

    Clean up routing rules

--- regex-server/src/Main.hs
+++ regex-server/src/Main.hs
@@ -1,10 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Control.Applicative
 import Control.Monad.IO.Class (liftIO)
 import Snap.Core
-import Snap.Util.FileServe
 import Snap.Http.Server
 import Regex
 --import System.IO.Temp (withTempFile)
@@ -16,21 +14,9 @@ import qualified Data.ByteString.UTF8 as UTF8
 main :: IO ()
 main =  quickHttpServe site
 
-render :: ByteString -> IO ByteString
-render input = let text = UTF8.toString input
-                   r = compile text
-                   dot = toDot r
-                   namedDot = setID (toGraphID text) dot
-               in graphvizWithHandle Dot namedDot Svg hGetContents
-
 site :: Snap ()
-site =
-    ifTop (writeBS "Hello world") <|>
-    route [ ("foo", writeBS "bar")
-          , ("echo/:echoparam", echoHandler)
-          , ("regex", serveImage)
-          ] <|>
-    dir "static" (serveDirectory "static")
+site = route [ ("regex", serveImage)
+             ]
 
 serveImage :: Snap ()
 serveImage = do Just [rawRegex] <- getsRequest $ rqQueryParam "r"
@@ -38,10 +24,11 @@ serveImage = do Just [rawRegex] <- getsRequest $ rqQueryParam "r"
                 writeBS image
                 modifyResponse $ setContentType svgContentType
                 modifyResponse $ setResponseCode 200
-  where svgContentType = "image/svg+xml"                  
+  where svgContentType = "image/svg+xml"
 
-echoHandler :: Snap ()
-echoHandler = do
-    param <- getParam "echoparam"
-    maybe (writeBS "must specify echo/param in URL")
-          writeBS param
+render :: ByteString -> IO ByteString
+render input = let text = UTF8.toString input
+                   r = compile text
+                   dot = toDot r
+                   namedDot = setID (toGraphID text) dot
+               in graphvizWithHandle Dot namedDot Svg hGetContents
